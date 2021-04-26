7de7eaa9467f28c7155eea19bee47a7564332ecf
eddie109109 <cjd_eddie@hotmail.com>
2021-04-25 23:11:26 -0700

    stuck on part 14

--- reddit.py
+++ reddit.py
@@ -11,12 +11,8 @@ reddit = praw.Reddit(
     user_agent = "eddie's user_agent for reddit",
 )
 
-titleList = []
-for submission in reddit.subreddit("learnpython").hot(limit=20):
-    # print(submission.title) #uncomment to see all the titles
-    titleList.append(submission.title)
-
-# for example: we will get 20 topics when we run the above function, and we are storing them in "titleList" for gensim to consume later on
+# Now we can scrape reddit by using the for loop below
+# for example: we will get 20 topics when we run the above function EVERYDAY, and we are storing them in "titleList" for gensim to consume later on
 # Ask Anything Monday - Weekly Thread
 # Tips on getting job as a python developer
 # Advice on how to recursively edit all the filenames in my directory with a specific extension to include the folder name, please!
@@ -37,6 +33,35 @@ for submission in reddit.subreddit("learnpython").hot(limit=20):
 # Putting data into dataframe by new line item?
 # Help with a challenge
 # Need help saving user input to a file
+
+# titleList = []
+# the below for loop is for scrape the data from reddit
+# for submission in reddit.subreddit("learnpython").hot(limit=20):
+#     # print(submission.title) #uncomment to see all the titles
+#     titleList.append(submission.title)
+
+# since i do not want my data to change every day, i have decided to use the follwing 20 topics as the list for this test run, 
+# we can uncomment the above function to scrape new data
+titleList = ["Ask Anything Monday - Weekly Thread",
+"Tips on getting job as a python developer",
+"Advice on how to recursively edit all the filenames in my directory with a specific extension to include the folder name, please!",
+"Send email notification when a script generates an error",
+"Looking for Dancing Link/Algorithm X hints",
+"Blockchain Voting",
+"Python calculator failed need help",
+"Help!! I'm trying to open a text file through python",
+"Anyone good at using Dash?",
+"Constraining a Random Walk in python, how to make it work?",
+"Only do something when a line isn't in any line of a text file",
+"Are there any books or videos that describe how to organize large projects?",
+"Extracting JSON",
+"A desperate request to help me run a python script",
+"I'm an idiot, how do I do linear regression with sklearn?",
+'"reading too much input" error on number guessing game',
+"Scraping Data from Video Game?",
+"Putting data into dataframe by new line item?",
+"Help with a challenge",
+"Need help saving user input to a file"]
 
 
 #########################################start setting up most of the packages below, you might need to install each of them one by one using pip3
@@ -148,3 +173,58 @@ print("After lemmatization: ", data_lemmatized[1:2])
 
 
 # 11. Create the Dictionary and Corpus needed for Topic Modeling
+# Create Dictionary
+id2word = corpora.Dictionary(data_lemmatized)
+
+# Create Corpus
+texts = data_lemmatized
+
+# Term Document Frequency
+corpus = [id2word.doc2bow(text) for text in texts]
+
+
+# View
+print("View the word frequency:", corpus[1:2])
+# Generate the following frequency list:
+# Based on [['tip', 'get', 'job', 'python', 'developer']]
+# [[(4, 1), (5, 1), (6, 1), (7, 1), (8, 1)]]
+
+
+# Gensim creates a unique id for each word in the document. The produced corpus shown above is a mapping of (word_id, word_frequency).
+# For example, (0, 1) above implies, word id 0 occurs once in the first document. Likewise, word id 1 occurs twice and so on.
+# This is used as the input by the LDA model.
+
+# Human readable format of corpus (term-frequency)
+[[(id2word[id], freq) for id, freq in cp] for cp in corpus[1:2]]
+
+
+# 12. Building the Topic Model
+
+# Build LDA model
+lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus,
+                                           id2word=id2word,
+                                           num_topics=20, 
+                                           random_state=100,
+                                           update_every=1,
+                                           chunksize=100,
+                                           passes=10,
+                                           alpha='auto',
+                                           per_word_topics=True)
+
+
+
+# 13. View the topics in LDA model
+
+# Print the Keyword in the 10 topics
+pprint(lda_model.print_topics())
+doc_lda = lda_model[corpus]  
+
+# 14. Compute Model Perplexity and Coherence Score
+# Compute Perplexity
+print('\nPerplexity: ', lda_model.log_perplexity(corpus))  # a measure of how good the model is. lower the better.
+
+# Compute Coherence Score
+coherence_model_lda = CoherenceModel(model=lda_model, texts=data_lemmatized, dictionary=id2word, coherence= "c_v")
+# coherence_lda = coherence_model_lda.get_coherence()
+## the above function "get_coherence()" of calculating the Coherence score is not working
+# print('\nCoherence Score: ', coherence_lda) 
